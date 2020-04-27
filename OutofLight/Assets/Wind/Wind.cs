57450bbbcb604a21c0914a0868a8bc672617680b
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-04-27 13:27:21 +0200

    Wind

---
+++ OutofLight/Assets/Wind/Wind.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour {
+
+    private ParticleSystem wind;
+
+    private float startTime;
+    private float endTime = 4;
+
+    void Awake() {
+        wind = GetComponent<ParticleSystem>();
+    }
+
+    void Update() {
+
+        startTime += Time.deltaTime;
+
+        if(startTime > endTime) {
+            wind.Play();
+            startTime = 0; 
+        }
+
+    }
+
+    void OnParticleCollision(GameObject other) {
+        if (other.CompareTag("Player")) {
+            print("Player");
+        }
+    }
+
+
+}
