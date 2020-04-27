04c245c575c51dddafe435fe834300cc905a7cc5
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-04-27 15:18:50 +0200

    qef

---
+++ OutofLight/Assets/Assets/Scripts/Misc/StepCounter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class StepCounter : MonoBehaviour {
+
+    public GameEvent stepsDepleted;
+    public BoolVariable safezone;
+    public IntVariable stepAmount;
+
+    [SerializeField]
+    private int startingStepsAmount;
+
+    void Awake() {
+        stepAmount.ChangeValue(-100);
+        stepAmount.ChangeValue(startingStepsAmount);
+    }   
+
+    public void DecrementStep() {
+        if (!safezone.IsTrue()) {
+            stepAmount.DecrementValue();
+        }
+        
+    }
+    
+    public void CheckIfStepsDepleted() {
+        if (stepAmount.GetValue() <= 0)
+            stepsDepleted.Raise();
+    }
+
+
+}
