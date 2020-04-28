b6f63719f3cacb0cf34873aa14374f5b7184c71a
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-04-28 15:59:46 +0200

    Slider

---
+++ OutofLight/Assets/LanternMeter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanternMeter : MonoBehaviour {
+
+    private Slider lanternSlider;
+
+    void Awake() {
+        lanternSlider = GetComponent<Slider>();
+    }
+
+}
