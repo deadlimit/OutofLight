5f48891ab248f5daa314b9a981d966527c288ebf
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-06 14:46:57 +0200

    Door to door

--- OutofLight/Assets/Scripts/Misc/Door.cs
+++ OutofLight/Assets/Scripts/Misc/Door.cs
@@ -7,6 +7,12 @@ public class Door : MonoBehaviour, IInteractable {
 
     public int newSceneIndex;
 
+    public GameEvent NewSceneLoad;
+
+    public SpawnPosition spawn;
+
+    public Vector3 otherSide;
+
     [SerializeField] private bool closeToDoor;
   
     public void IsCloseToDoor(bool value) {
@@ -14,8 +20,13 @@ public class Door : MonoBehaviour, IInteractable {
     }
 
     public void Use() {
-        if(closeToDoor)
-            SceneManager.LoadScene(newSceneIndex);  
+        if (closeToDoor) {
+            NewSceneLoad.Raise();
+            spawn.spawnPosition = otherSide;
+            SceneManager.LoadScene(newSceneIndex);
+        }
+            
+                  
     }
 
 
