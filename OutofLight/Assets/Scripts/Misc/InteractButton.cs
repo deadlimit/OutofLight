cdf3c01591e46de0dada1352b049c0a110b89594
krli8455 <kristoffer.lindroth92@gmail.com>
2020-05-11 14:07:35 +0200

    smol update

---
+++ OutofLight/Assets/Scripts/Misc/InteractButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractButton : MonoBehaviour
+{
+    public Button interact;
+
+    private TouchInput input;
+
+    private void Start()
+    {
+        interact.enabled = false;
+    }
+
+    private void Interact()
+    {
+        if (interact.enabled){
+            
+        }
+    }
+}
