d51bc21a2ef221261b188ffa3275d756d7dce176
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-13 22:07:57 +0200

    Så

---
+++ OutofLight/Assets/Scripts/Misc/InteractButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractButton : MonoBehaviour {
+    
+    private Button thisButton;
+    private Image thisImage;
+
+    public InteractParameter interact;
+    
+    private void Awake() {
+        thisImage = GetComponent<Image>();
+        thisImage.enabled = false;
+        thisButton = GetComponent<Button>();
+    }
+    
+    public void SetNewInteract() {
+        thisImage.enabled = true;
+        thisButton.onClick.AddListener(Interact);
+    }
+
+    private void Interact() {
+        interact.thisObject.Use();
+        thisImage.enabled = false;
+    }
+    
+    public void Reset() {
+        thisButton.onClick.RemoveAllListeners();
+        thisImage.enabled = false;
+    }
+
+
+
+}
