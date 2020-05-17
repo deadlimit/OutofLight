16b6e82b80377d7a1ee7c0eeec81908207858eab
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-17 22:37:21 +0200

    Började på inventory

---
+++ OutofLight/Assets/Inventory/Scripts/Inventory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Inventory : ScriptableObject {
+
+    public List<Item> items = new List<Item>();
+    
+}
---
+++ OutofLight/Assets/Inventory/Scripts/Item.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu]
+public class Item : ScriptableObject {
+
+    public string name;
+    public string description;
+    public Image image;
+
+}
