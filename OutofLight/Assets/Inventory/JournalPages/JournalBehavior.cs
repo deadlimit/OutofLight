660b8ae8d4fccaa9238ea1009d20287a104ea5c9
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-18 15:09:36 +0200

    SO Journal

---
+++ OutofLight/Assets/Inventory/JournalPages/JournalBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JournalBehavior : MonoBehaviour, IInteractable {
+
+    public GameObject journalUIPrefab;
+    public JournalPage page;
+    public Journal journal;
+    public void Use() {
+        
+        journal.Add(page);
+        Destroy(gameObject);
+    }
+}
---
+++ OutofLight/Assets/Inventory/Scripts/Templates/Journal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Inventory/Journal")]
+public class Journal : ScriptableObject {
+
+    public int pages;
+
+    public JournalPage[] journal;
+
+    public void Add(JournalPage page) {
+        journal[page.journalPageEntry] = page;
+    }
+    
+    public void OnEnable() {
+        journal = new JournalPage[pages];
+    }
+}
