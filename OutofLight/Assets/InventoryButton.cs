3b137fa5d954eea3c2a066d6c5ee54038899ea64
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-18 16:56:21 +0200

    wef

---
+++ OutofLight/Assets/InventoryButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryButton : MonoBehaviour {
+
+    public void OpenInventory()
+    {
+        print("Inventory");
+    }
+    
+}
---
+++ OutofLight/Assets/UIJournal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIJournal : MonoBehaviour
+{
+
+    public Journal journal;
+    public Button[] slots;
+    public Text[] slotTexts;
+    
+    private void Awake()
+    {
+        slots = GetComponentsInChildren<Button>();
+        slotTexts = GetComponentsInChildren<Text>();
+    }
+
+}
