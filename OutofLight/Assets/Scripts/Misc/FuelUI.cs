ac3d90b557f9588b7b29bef409373150ec836e8f
krli8455 <kristoffer.lindroth92@gmail.com>
2020-05-19 17:20:59 +0200

    Öppdejt UI 3.0 prefab

--- OutofLight/Assets/Scripts/Misc/FuelUI.cs
+++ OutofLight/Assets/Scripts/Misc/FuelUI.cs
@@ -7,18 +7,43 @@ public class FuelUI : MonoBehaviour
 {
     public IntVariable stepAmount;
     public Slider lanternSlider;
-    public float lerpSpeed;
+    public float lerpSpeed, lerpColor;
+    public Image fill;
+    public Color high, middle, low, currentColor;
+
+    private float middleValue, lowValue;
 
     private void Start()
     {
         lanternSlider = lanternSlider.GetComponent<Slider>();
         lanternSlider.maxValue = stepAmount.GetValue();
         lanternSlider.value = stepAmount.GetValue();
+        SetColors();
     }
 
     private void Update()
     {
         lanternSlider.value = Mathf.Lerp(lanternSlider.value, stepAmount.GetValue(), Time.deltaTime * lerpSpeed);
+        if (lanternSlider.value > middleValue)
+        {
+            currentColor = Color.Lerp(currentColor, high, Time.deltaTime * lerpColor);
+        } 
+        if (lanternSlider.value > lowValue && lanternSlider.value < middleValue)
+        {
+            currentColor = Color.Lerp(currentColor, middle, Time.deltaTime * lerpColor);
+        }
+        else if (lanternSlider.value < lowValue)
+        {
+            currentColor = Color.Lerp(currentColor, low, Time.deltaTime * lerpColor);
+        }
+
+        fill.color = currentColor;
+    }
+
+    private void SetColors()
+    {
+        middleValue = lanternSlider.maxValue * 0.75f;
+        lowValue = lanternSlider.maxValue * 0.3f;
     }
 
 }
