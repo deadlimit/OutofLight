b7aa3ecbe13ce6f35318ce5498f33a4598541ab4
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-19 12:04:07 +0200

    Animerade JournalPage

--- OutofLight/Assets/Inventory/JournalPages/JournalHandler.cs
+++ OutofLight/Assets/Inventory/JournalPages/JournalHandler.cs
@@ -10,6 +10,8 @@ public class JournalHandler : MonoBehaviour {
     public Text entry;
     
     public void Awake() {
+	    transform.localScale = Vector3.zero;
+	    LeanTween.scale(gameObject, new Vector3(1, 1, 1), .5f);
 	    var page = journal.latest;
         if (page == null) return; 
         day.text = page.day;
@@ -18,8 +20,11 @@ public class JournalHandler : MonoBehaviour {
     }
     
     public void Close() { 
-	    anim.SetTrigger("Close");
-       Destroy(gameObject, 1.01f);
+	    LeanTween.scale(gameObject, Vector3.zero, .1f).setOnComplete(DestroyThis);
+    }
+
+    private void DestroyThis() {
+	    Destroy(gameObject);
     }
     
 }
