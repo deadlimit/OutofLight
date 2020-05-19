86ef3a555e677d7727754957ac83de81a5a53c86
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-19 16:25:05 +0200

    Håller på¨med stairs

--- OutofLight/Assets/Scripts/Tile/Tile.cs
+++ OutofLight/Assets/Scripts/Tile/Tile.cs
@@ -16,25 +16,9 @@ public class Tile : MonoBehaviour {
         mesh = GetComponent<MeshRenderer>();
     }
 
-
     public void HighlightTile(Color color) {
-        Debug.Log(gameObject);
         mesh.material.color = color;
     }
 
-    public void Turn() {
-        if (isTrapTile)
-            StartCoroutine(Rotate());
-            
-        
-    }
-
-    private IEnumerator Rotate() {
-        var targetAngle = transform.eulerAngles + 180 * Vector3.down;
-        while(transform.rotation.z != 180) {
-            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngle, Time.deltaTime * 10);
-            yield return null;
-        }
-    }
    
 }
---
+++ OutofLight/Assets/Stair.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering.Universal.ShaderGUI;
+using UnityEngine;
+using UnityEngine.Experimental.TerrainAPI;
+
+public class Stair : MonoBehaviour, IInteractable {
+	
+
+	private void Awake() {
+		ChangeLayerMask("Default");
+	}
+	
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Player")) { 
+			ChangeLayerMask("Interactable");
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.gameObject.CompareTag("Player")) {
+			ChangeLayerMask("Default");
+		}
+	}
+	
+	public void Use() {
+		Debug.Log("Stair");
+	}
+
+	private void ChangeLayerMask(string arg) {
+		gameObject.layer = LayerMask.NameToLayer(arg);
+	}
+
+}
