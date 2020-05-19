f655da30febbed13901b900ae49f2eb6a7331c28
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-19 11:15:32 +0200

    RoomPresenter

--- OutofLight/Assets/RoomPresenter.cs
+++ OutofLight/Assets/RoomPresenter.cs
@@ -16,19 +16,9 @@ public class RoomPresenter : MonoBehaviour {
 
 	private void Start() {
 		
-		StartCoroutine(FadeTo(255, 3));
+		
 	}
 	
 	
-	private IEnumerator FadeTo(float value, float time) {
-		var alpha = sceneName.color.a;
-		for (var t = 0.0f; t < 1.0f; t += Time.deltaTime / time) {
-			var newColor = new Color(sceneName.color.r, sceneName.color.g, sceneName.color.b,
-				Mathf.Lerp(alpha, value, t));
-			sceneName.color = newColor;
-			yield return null;
-		}
-		
-		
-	}
+
 }
--- OutofLight/Assets/Scripts/Misc/UINewRoom.cs
+++ OutofLight/Assets/Scripts/Misc/UINewRoom.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UINewRoom : MonoBehaviour {
     
     public Image fadeImage;
-    public GameObject roomPresenter;
+    public Text room;
+
+    public float fadeTime;
+    public float waitBeforeTextFade;
+
     private void Start() {
+        room.text = SceneManager.GetActiveScene().name;
         fadeImage.enabled = true;
-        StartCoroutine(FadeTo(0, 2));
+        StartCoroutine(ShowSequence());
+
+    }
+
+    private IEnumerator ShowSequence() {
+        Debug.Log("Fade image");
+        StartCoroutine(ImageFade(0, fadeTime));
+        yield return new WaitForSeconds(fadeTime);
+        Debug.Log("Fade in Text");
+        StartCoroutine(SceneText(1, fadeTime));
+        yield return new WaitForSeconds(fadeTime + waitBeforeTextFade);
+        Debug.Log("Fade out Text");
+        StartCoroutine(SceneText(0, fadeTime));
+        yield return new WaitForSeconds(fadeTime);
+        Destroy(gameObject);
     }
     
-    private IEnumerator FadeTo(float value, float time) {
+    private IEnumerator ImageFade(float value, float time) {
         var alpha = fadeImage.color.a;
         for (var t = 0.0f; t < 1.0f; t += Time.deltaTime / time) {
             var newColor = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b,
@@ -19,8 +39,17 @@ public class UINewRoom : MonoBehaviour {
             fadeImage.color = newColor;
             yield return null;
         }
-        
-        Instantiate(roomPresenter, transform.position + (Vector3.up * 100) , Quaternion.identity, transform);
+    }
+    
+    private IEnumerator SceneText(float value, float time) {
+        var alpha = room.color.a;
+        for (var t = 0.0f; t < 1.0f; t += Time.deltaTime / time) {
+            var newColor = new Color(room.color.r, room.color.g, room.color.b,
+                Mathf.Lerp(alpha, value, t));
+            room.color = newColor;
+            yield return null;
+        }
+        yield return new WaitForSeconds(waitBeforeTextFade);
     }
     
 }
