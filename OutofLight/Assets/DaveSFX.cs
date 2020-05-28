bab6cab8be130e52d1f0a6a3796f2fd04bf85625
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-28 23:59:28 +0200

    Dave fotsteg

---
+++ OutofLight/Assets/DaveSFX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaveSFX : MonoBehaviour {
+
+	public AudioSource audio;
+
+	public void Awake() {
+		audio = GetComponent<AudioSource>();
+	}
+	
+	public void Play(AudioClip clip) {
+		audio.PlayOneShot(clip);
+	}
+
+}
