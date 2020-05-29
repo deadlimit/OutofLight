0a271535268c1a8742c7536ac2d5d5a7a0f49ed4
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-29 11:11:23 +0200

    Spöket kan inte gå på safezone tiles längre

--- OutofLight/Assets/Scripts/Tile/Tile.cs
+++ OutofLight/Assets/Scripts/Tile/Tile.cs
@@ -6,34 +6,17 @@ using UnityEngine.Events;
 
 public class Tile : MonoBehaviour {
 
-    private MeshRenderer mesh;
-
+    public bool safezoneTile;
+    
     private float x;
     private float y;
-
-    public int gCost;
-    public int hCost;
-    
-    
-    private void Awake() {
-        x = transform.position.x;
-        y = transform.position.z;
-        mesh = GetComponent<MeshRenderer>();
-    }
-
-    public void HighlightTile(Color color) {
-        mesh.material.color = color;
-    }
     
     public override bool Equals(object other) {
         return other is Tile tile && tile == this;
     }
 
-    public int fCost() {
-        return gCost + hCost;
+    public bool IsSafezoneTile() {
+        return safezoneTile;
     }
-
-    public override int GetHashCode() {
-        return (int)(x * y + 2);
-    }
+    
 }
