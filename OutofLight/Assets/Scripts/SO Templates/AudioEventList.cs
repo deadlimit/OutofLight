5ca0c50919239ed84dc717945b2f52f107ef5dac
krli8455 <kristoffer.lindroth92@gmail.com>
2020-05-30 17:58:40 +0200

    Uppdaterat Cellar + Movable Box

---
+++ OutofLight/Assets/Scripts/SO Templates/AudioEventList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AudioEventList : ScriptableObject
+{
+    public List<GameEvent> events = new List<GameEvent>();
+    public AudioClip assosiatedSound;
+}
