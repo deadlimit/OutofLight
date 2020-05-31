6710204a8a8ee793ade8445b10977b7b87bfec63
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-05-31 17:19:29 +0200

    Balcony peakevent och door fungerar

---
+++ OutofLight/Assets/PeakWindowEvent.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PeakWindowEvent : MonoBehaviour, IInteractable {
+
+	public CinemachineVirtualCamera peakCamera;
+	public Button button;
+	public Material ghostShader;
+	public GameObject ghostFlame;
+
+	public float shaderSpeed;
+	public float waitBeforeShowingGhost;
+
+	public AudioSource audio;
+	
+	private void Awake() {
+		ghostFlame.gameObject.SetActive(false);
+		peakCamera.gameObject.SetActive(false);
+		StartCoroutine(ChangeShader(3));
+	}
+	
+	public void Use() {
+		StartCoroutine(PeakSequence());
+	}
+
+	private IEnumerator PeakSequence() {
+		peakCamera.gameObject.SetActive(true);
+		yield return new WaitForSeconds(waitBeforeShowingGhost);
+		StartCoroutine(ChangeShader(-3));
+		audio.Play();
+		yield return new WaitForSeconds(1);
+		StartCoroutine(ChangeShader(3));
+		peakCamera.gameObject.SetActive(false);
+	}
+	
+	public string GetPrompt() {
+		return "Peak inside";
+	}
+
+	public Button CustomSprite() {
+		return button;
+	}
+	
+	private IEnumerator ChangeShader(int value) {
+		float start = 0;
+		float end = 1;
+		while (start < end) {
+			float changeValue = Mathf.Lerp(ghostShader.GetFloat("ShaderController"), value, Time.deltaTime * shaderSpeed);
+			ghostShader.SetFloat("ShaderController", changeValue);
+			start += Time.deltaTime;
+			yield return null;
+		}
+	}
+}
