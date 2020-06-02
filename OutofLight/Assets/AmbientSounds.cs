cd6a2b7c878117538ac6bfd091965203a9918068
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-06-02 10:40:03 +0200

    AmbientSoundController

---
+++ OutofLight/Assets/AmbientSounds.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientSounds : MonoBehaviour {
+
+    
+    
+    
+    public List<AudioClip> soundClips = new List<AudioClip>();
+
+    private AudioSource audio;
+
+    private float audioClipLength;
+    private float startTime;
+    private float randomWaitTime;
+    
+    private void Awake() {
+        startTime = 0;
+        randomWaitTime = Random.Range(5, 10);
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void Update() {
+
+        startTime += Time.deltaTime;
+
+        if (!audio.isPlaying && startTime > randomWaitTime) {
+            startTime = 0;
+            randomWaitTime = Random.Range(5, 10) + audioClipLength;
+            print(randomWaitTime);
+            audio.PlayOneShot(GetAudioClip());
+        }
+        
+    }
+
+    private AudioClip GetAudioClip() {
+        int randomIndex = Random.Range(0, soundClips.Count);
+        var clip = soundClips[randomIndex];
+        print(clip);
+        audioClipLength = clip.length;
+        return clip;
+    }
+    
+    
+
+}
