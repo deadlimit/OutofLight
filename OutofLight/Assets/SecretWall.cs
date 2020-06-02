42b451bc115473a2b688eb1ffa23ff6a2fe7adb1
deadlimit <47717668+deadlimit@users.noreply.github.com>
2020-06-02 11:42:12 +0200

    Vad fan som helst

--- OutofLight/Assets/SecretWall.cs
+++ OutofLight/Assets/SecretWall.cs
@@ -8,16 +8,23 @@ public class SecretWall : MonoBehaviour, ILever {
 	public Transform wallOne;
 
 	public float newY;
+	public float backY;
+
+	public AudioSource audio;
 	
 	public void RotateWalls() {
 		wallOne.LeanRotateY(newY, 2);
 	}
 
 	public void RotateBack() {
-		wallOne.LeanRotateY(0, 2);
+		if(audio != null)
+			audio.Play();
+		wallOne.LeanRotateY(backY, 2);
 	}
 
 	public void Method() {
+		if(audio != null)
+			audio.Play();
 		RotateWalls();
 	}
 }
