1e1295179b9dcbc1500846fbbd59423fb598f738
krli8455 <kristoffer.lindroth92@gmail.com>
2020-06-06 00:52:35 +0200

    FuelBar siffror undertill + lite fixes (bla fungerande dörr Balcony - West Wing

---
+++ OutofLight/Assets/Scripts/UI/UIFuelText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIFuelText : MonoBehaviour
+{
+    public Text fuelBarT;
+    public Slider fuelSlider;
+    public IntVariable stepAmount;
+    public Scene currentscene;
+    public FuelUI fuel;
+    private void Awake()
+    {
+        currentscene = SceneManager.GetActiveScene();
+    }
+    private void FixedUpdate()
+    {
+        fuelBarT.text = stepAmount.GetValue().ToString() + "/" + fuelSlider.maxValue.ToString();
+        fuelBarT.color = fuel.currentColor;
+        if (currentscene == SceneManager.GetSceneByBuildIndex(1) || currentscene == SceneManager.GetSceneByBuildIndex(5) || currentscene == SceneManager.GetSceneByBuildIndex(6))
+        {
+            fuelBarT.text = "-";
+        }
+    }
+
+    
+
+}
