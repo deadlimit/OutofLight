b545bfa104ac242bb555fe50db24a40511db34f4
krli8455 <kristoffer.lindroth92@gmail.com>
2020-06-06 20:44:56 +0200

    La till UIParticleEffects

---
+++ OutofLight/Assets/Scripts/Misc/CheatToCellar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatToCellar : MonoBehaviour
+{
+    public RequirementManager req;
+
+    public List<string> requirementName = new List<string>();
+
+    public Dictionary<string, bool> requirementList = new Dictionary<string, bool>();
+
+    private void Awake()
+    {
+        foreach (var b in requirementList)
+        {
+
+        }
+    }
+
+}
